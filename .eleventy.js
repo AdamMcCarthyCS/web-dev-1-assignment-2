2bcfbf6ee247c4099c5b6f3afb8d8ec343fa83b1
AdamMcCarthyCS <adammccarthycs@gmail.com>
2025-12-26 18:53:46 +0000

    chore(js-yaml): add js-yaml to project for rendering data for pagination

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+const yaml = require("js-yaml");
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
 };
